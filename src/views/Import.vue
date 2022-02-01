5650c43b693ac12955286e90b210b4c35e106da7
Peterliang233 <ncuyanping666@126.com>
2022-02-01 22:12:53 +0800

    :sparkles: 基本实现所有部分，对添加队伍的界面实现了数据的同步展示操作

--- src/views/Import.vue
+++ src/views/Import.vue
@@ -3,7 +3,7 @@
     <el-header><Head/></el-header>
     <div style="height: 10%; width: 100%">
       <div style="float: right;">
-        <el-button type="primary"><span>添加队伍</span></el-button>
+        <el-button type="primary" @click="addTeam"><span>添加队伍</span></el-button>
       </div>
     </div>
     <div style="height: 2%"></div>
@@ -43,6 +43,40 @@ export default {
   methods: {
     getTeams: function() {
       return this.$store.getters.getTeamsName
+    },
+    addTeam: function() {
+      this.$prompt('请输入队伍名', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(({ value }) => {
+          // 将数据传输到后端进行持久化
+          this.CreateName(value)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        });
+      });
+    },
+    CreateName: function (name) {
+      this.$axios.post('/count-tool/game/team/new?name='+name).
+      then(res=>{
+        if(res.data.status===200){
+          // 现在就是要重新进行一个渲染当前的界面了
+          let obj = {}
+          obj.name=name
+          this.tableData.push(obj)
+          this.$message({
+            type: 'success',
+            message: `添加成功`,
+          })
+        }
+      }).catch(error=>{
+        this.$message({
+          type: 'error',
+          message: `添加失败`,
+        });
+      })
     }
   }
 }
